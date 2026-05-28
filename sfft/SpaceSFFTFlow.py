a5d13f08e86e8152468aacd3ca73e7b9402fd03a
Michael Wood-Vasey <wmwv@pitt.edu>
2026-05-28 14:52:04 -0400

    black format

--- sfft/SpaceSFFTFlow.py
+++ sfft/SpaceSFFTFlow.py
@@ -11,22 +11,38 @@ from sfft.utils.SFFTSolutionReader import Realize_MatchingKernel
 __last_update__ = "2025-05-26"
 __author__ = "Lei Hu <leihu@andrew.cmu.edu> and Michael Wood-Vasey <wmwv@pitt.edu>"
 
-class SpaceSFFT_CupyFlow:
+
+class SpaceSFFT_Flow:
     """Run A Cupy WorkFlow for SFFT subtraction"""
 
-    def __init__(self, hdr_target, hdr_object, 
-                 target_skyrms, object_skyrms, 
-                 PixA_target, PixA_object,
-                 PixA_targetVar, PixA_objectVar,
-                 PixA_target_DMASK, PixA_object_DMASK, 
-                 PSF_target, PSF_object,
-                 sci_is_target=True,
-                 GKerHW=9, KerPolyOrder=2, BGPolyOrder=0, ConstPhotRatio=True, 
-                 Consider_Matching_Kernel=False,
-                 BACKEND_4SUBTRACT="Cupy",
-                 CUDA_COMPILER="nvrtc", CUDA_DEVICE_4SUBTRACT='0',
-                 NUM_CPU_THREADS_4SUBTRACT=8, NUMBA_CACHE=True,
-                 GAIN=1.0, RANDOM_SEED=10086):
+    def __init__(
+        self,
+        hdr_target,
+        hdr_object,
+        target_skyrms,
+        object_skyrms,
+        PixA_target,
+        PixA_object,
+        PixA_targetVar,
+        PixA_objectVar,
+        PixA_target_DMASK,
+        PixA_object_DMASK,
+        PSF_target,
+        PSF_object,
+        sci_is_target=True,
+        GKerHW=9,
+        KerPolyOrder=2,
+        BGPolyOrder=0,
+        ConstPhotRatio=True,
+        Consider_Matching_Kernel=False,
+        BACKEND_4SUBTRACT="Cupy",
+        CUDA_COMPILER="nvrtc",
+        CUDA_DEVICE_4SUBTRACT="0",
+        NUM_CPU_THREADS_4SUBTRACT=8,
+        NUMBA_CACHE=True,
+        GAIN=1.0,
+        RANDOM_SEED=10086,
+    ):
         """Do things.
 
         Parameters
@@ -57,11 +73,11 @@ class SpaceSFFT_CupyFlow:
               2d image variance of original image, indexed by x, y.
 
            PixA_objectVar: cupy array (float64)
-              2d image variance of original image, indexed by x, y. 
+              2d image variance of original image, indexed by x, y.
 
            PixA_target_DMASK: cupy array (bool)
               2d detection mask for target image
-        
+
            PixA_object_DMASK: cupy array (bool)
               2d detection mask for unresampled object image
 
@@ -77,7 +93,7 @@ class SpaceSFFT_CupyFlow:
 
            GKerHW: int
               Matching kernel half-width (full width is 2*GkerHW + 1 )
-           
+
            KerPolyOrder: int
               Order of spatial variation in kernel
 
@@ -91,7 +107,7 @@ class SpaceSFFT_CupyFlow:
 
            Consider_Matching_Kernel: bool, default False
               Whether to consider the matching kernel in the decorrelation.
-              The mathching kernel is close to a delta function doing a shift, 
+              The matching kernel is close to a delta function doing a shift,
               by default, we may ignore it.
 
            CUDA_DEVICE_4SUBTRACT: str, default '0'
@@ -107,14 +123,14 @@ class SpaceSFFT_CupyFlow:
 
         """
 
-        assert PixA_target.flags['C_CONTIGUOUS']
-        assert PixA_object.flags['C_CONTIGUOUS']
-        
-        assert PixA_target_DMASK.flags['C_CONTIGUOUS']
-        assert PixA_object_DMASK.flags['C_CONTIGUOUS']
-        
-        assert PSF_target.flags['C_CONTIGUOUS']
-        assert PSF_object.flags['C_CONTIGUOUS']
+        assert PixA_target.flags["C_CONTIGUOUS"]
+        assert PixA_object.flags["C_CONTIGUOUS"]
+
+        assert PixA_target_DMASK.flags["C_CONTIGUOUS"]
+        assert PixA_object_DMASK.flags["C_CONTIGUOUS"]
+
+        assert PSF_target.flags["C_CONTIGUOUS"]
+        assert PSF_object.flags["C_CONTIGUOUS"]
 
         self.hdr_target = hdr_target
         self.hdr_object = hdr_object
@@ -220,7 +236,7 @@ class SpaceSFFT_CupyFlow:
         else:
             raise ValueError("Unsupported BACKEND_4SUBTRACT '%s'" % self.BACKEND_4SUBTRACT)
 
-    def resampling_image_mask_psf( self ):
+    def resampling_image_mask_psf(self):
         """Step 0. run resampling for input object image, variance image, mask, and PSF"""
         if self.BACKEND_4SUBTRACT == "Cupy":
             CR = self.op.Resampling(RESAMP_METHOD="BILINEAR", VERBOSE_LEVEL=1)
@@ -250,50 +266,44 @@ class SpaceSFFT_CupyFlow:
         # TODO: this check is currently not smart...
         NTX = int(self.hdr_target["NAXIS1"])
         NTY = int(self.hdr_target["NAXIS2"])
-        NPIX_INNER = self.op_sum(self.op_logical_and( self.op_logical_and(XX_proj >= 0.5, XX_proj < NTX+0.5),
-                                            self.op_logical_and(YY_proj >= 0.5, YY_proj < NTY+0.5) ))
+        NPIX_INNER = self.op.sum(
+            self.op.logical_and(
+                self.op.logical_and(XX_proj >= 0.5, XX_proj < NTX + 0.5),
+                self.op.logical_and(YY_proj >= 0.5, YY_proj < NTY + 0.5),
+            )
+        )
         assert NPIX_INNER > 0, "SFFT Error: Projection of object image is completely outside of target image!"
 
         # Object image:
-        PixA_Eobj, EProjDict = CR.frame_extension(XX_proj=XX_proj,
-                                                      YY_proj=YY_proj, 
-                                                      PixA_obj=self.PixA_object,
-                                                      PAD_FILL_VALUE=0.,
-                                                      NAN_FILL_VALUE=0.)
+        PixA_Eobj, EProjDict = CR.frame_extension(
+            XX_proj=XX_proj, YY_proj=YY_proj, PixA_obj=self.PixA_object, PAD_FILL_VALUE=0.0, NAN_FILL_VALUE=0.0
+        )
 
-        self.PixA_resamp_object = CR.resampling(PixA_Eobj=PixA_Eobj,
-                                                    EProjDict=EProjDict,
-                                                    CUDA_COMPILER=self.CUDA_COMPILER,
-                                                    USE_SHARED_MEMORY=False)
+        self.PixA_resamp_object = CR.resampling(
+            PixA_Eobj=PixA_Eobj, EProjDict=EProjDict, CUDA_COMPILER=self.CUDA_COMPILER, USE_SHARED_MEMORY=False
+        )
 
         # Variance image:
-        PixA_EobjVar, EProjDict_Var = CR.frame_extension(XX_proj=XX_proj,
-                                                             YY_proj=YY_proj, 
-                                                             PixA_obj=self.PixA_objectVar,
-                                                             PAD_FILL_VALUE=0.,
-                                                             NAN_FILL_VALUE=0.)
+        PixA_EobjVar, EProjDict_Var = CR.frame_extension(
+            XX_proj=XX_proj, YY_proj=YY_proj, PixA_obj=self.PixA_objectVar, PAD_FILL_VALUE=0.0, NAN_FILL_VALUE=0.0
+        )
 
-        self.PixA_resamp_objectVar = CR.resampling(PixA_Eobj=PixA_EobjVar,
-                                                       EProjDict=EProjDict,
-                                                       CUDA_COMPILER=self.CUDA_COMPILER,
-                                                       USE_SHARED_MEMORY=False)
+        self.PixA_resamp_objectVar = CR.resampling(
+            PixA_Eobj=PixA_EobjVar, EProjDict=EProjDict, CUDA_COMPILER=self.CUDA_COMPILER, USE_SHARED_MEMORY=False
+        )
 
         # Mask:
-        PixA_Eobj, EProjDict = CR.frame_extension(XX_proj=XX_proj,
-                                                      YY_proj=YY_proj, 
-                                                      PixA_obj=self.PixA_object_DMASK,
-                                                      PAD_FILL_VALUE=0.,
-                                                      NAN_FILL_VALUE=0.)
+        PixA_Eobj, EProjDict = CR.frame_extension(
+            XX_proj=XX_proj, YY_proj=YY_proj, PixA_obj=self.PixA_object_DMASK, PAD_FILL_VALUE=0.0, NAN_FILL_VALUE=0.0
+        )
 
         del XX_proj
         del YY_proj
-        
-        self.PixA_resamp_object_DMASK = CR.resampling(PixA_Eobj=PixA_Eobj,
-                                                          EProjDict=EProjDict,
-                                                          CUDA_COMPILER=self.CUDA_COMPILER,
-                                                          USE_SHARED_MEMORY=False)
-        self.BlankMask = self.PixA_resamp_object == 0.
 
+        self.PixA_resamp_object_DMASK = CR.resampling(
+            PixA_Eobj=PixA_Eobj, EProjDict=EProjDict, CUDA_COMPILER=self.CUDA_COMPILER, USE_SHARED_MEMORY=False
+        )
+        self.BlankMask = self.PixA_resamp_object == 0.0
 
         # PSF:
         PATTERN_ROTATE_ANGLE = self.op.PRC(hdr_obj=self.hdr_object, hdr_targ=self.hdr_target)
@@ -351,21 +361,21 @@ class SpaceSFFT_CupyFlow:
             self.PixA_target_DMASK == 0, self.PixA_resamp_object_DMASK < 0.1
         )  # background-mask
 
-        NaNmask_Ctarget = self.op_isnan(self.PixA_Ctarget)
-        NaNmask_Cresamp_object = self.op_isnan(self.PixA_Cresamp_object)
+        NaNmask_Ctarget = self.op.isnan(self.PixA_Ctarget)
+        NaNmask_Cresamp_object = self.op.isnan(self.PixA_Cresamp_object)
         if NaNmask_Ctarget.any() or NaNmask_Cresamp_object.any():
-            NaNmask = self.op_logical_or(NaNmask_Ctarget, NaNmask_Cresamp_object)
-            ZeroMask = self.op_logical_or(NaNmask, LYMASK_BKG)
+            NaNmask = self.op.logical_or(NaNmask_Ctarget, NaNmask_Cresamp_object)
+            ZeroMask = self.op.logical_or(NaNmask, LYMASK_BKG)
         else:
             ZeroMask = LYMASK_BKG
 
         del LYMASK_BKG
-            
+
         PixA_mCtarget = self.PixA_Ctarget.copy()
-        PixA_mCtarget[ZeroMask] = 0.
+        PixA_mCtarget[ZeroMask] = 0.0
 
         PixA_mCresamp_object = self.PixA_Cresamp_object.copy()
-        PixA_mCresamp_object[ZeroMask] = 0.
+        PixA_mCresamp_object[ZeroMask] = 0.0
 
         del ZeroMask
 
@@ -380,19 +390,19 @@ class SpaceSFFT_CupyFlow:
             PixA_SCI = self.PixA_Cresamp_object
             PixA_mREF = PixA_mCtarget
             PixA_mSCI = PixA_mCresamp_object
-        
+
         if self.BACKEND_4SUBTRACT == "Cupy":
-            self.Solution, self.PixA_DIFF = self.PCCP(
+            self.Solution, self.PixA_DIFF = self.op.PCCP(
                 PixA_REF=PixA_REF,
                 PixA_SCI=PixA_SCI,
                 PixA_mREF=PixA_mREF,
                 PixA_mSCI=PixA_mSCI,
-                ForceConv='REF' if self.sci_is_target else 'NEW',
+                ForceConv="REF" if self.sci_is_target else "NEW",
                 GKerHW=self.GKerHW,
                 KerPolyOrder=self.KerPolyOrder,
                 BGPolyOrder=self.BGPolyOrder,
-                ConstPhotRatio=self.ConstPhotRatio, 
-                CUDA_DEVICE_4SUBTRACT=self.CUDA_DEVICE_4SUBTRACT
+                ConstPhotRatio=self.ConstPhotRatio,
+                CUDA_DEVICE_4SUBTRACT=self.CUDA_DEVICE_4SUBTRACT,
             )
         elif self.BACKEND_4SUBTRACT == "Numpy":
             NX, NY = PixA_REF.shape
@@ -421,104 +431,109 @@ class SpaceSFFT_CupyFlow:
             )
         else:
             raise ValueError("Unsupported BACKEND_4SUBTRACT '%s'" % self.BACKEND_4SUBTRACT)
-    
-        self.PixA_DIFF[self.BlankMask] = 0.
 
-    def find_decorrelation( self ):
+        self.PixA_DIFF[self.BlankMask] = 0.0
+
+    def find_decorrelation(self):
         """Step 3. perform decorrelation in Fourier domain
         extract matching kernel at the center
         """
         N0, N1 = self.PixA_DIFF.shape
-        L0, L1 = 2*self.GKerHW + 1, 2*self.GKerHW + 1
+        L0, L1 = 2 * self.GKerHW + 1, 2 * self.GKerHW + 1
         DK = self.KerPolyOrder
-        Fpq = int((self.BGPolyOrder+1)*(self.BGPolyOrder+2)/2)
-        XY_q = np.array([[N0/2.+0.5, N1/2.+0.5]])
+        Fpq = int((self.BGPolyOrder + 1) * (self.BGPolyOrder + 2) / 2)
+        XY_q = np.array([[N0 / 2.0 + 0.5, N1 / 2.0 + 0.5]])
 
-        self.Solution = self.op_asnumpy(self.Solution)
-        MATCH_KERNEL = self.op_array(Realize_MatchingKernel(XY_q=XY_q).FromArray(
-            Solution=self.Solution, N0=N0, N1=N1, L0=L0, L1=L1, DK=DK, Fpq=Fpq
-        )[0], dtype=np.float64)
-        self.MATCH_KERNEL = self.op_asnumpy(MATCH_KERNEL)
+        self.Solution = self.op.asnumpy(self.Solution)
+        MATCH_KERNEL = self.op.array(
+            Realize_MatchingKernel(XY_q=XY_q).FromArray(
+                Solution=self.Solution, N0=N0, N1=N1, L0=L0, L1=L1, DK=DK, Fpq=Fpq
+            )[0],
+            dtype=np.float64,
+        )
+        self.MATCH_KERNEL = self.op.asnumpy(MATCH_KERNEL)
 
         if self.Consider_Matching_Kernel:
-            MK = self.op_asnumpy(MATCH_KERNEL)
+            MK = self.op.asnumpy(MATCH_KERNEL)
         else:
             MK = None
-        self.FKDECO = DeCorrelation_Calculator(NX_IMG=N0, 
-                                               NY_IMG=N1, 
-                                               KERNEL_JQueue=[self.op_asnumpy(self.PSF_resamp_object)], 
-                                               BKGSIG_JQueue=[self.target_skyrms], 
-                                               KERNEL_IQueue=[self.op_asnumpy(self.PSF_target)], 
-                                               BKGSIG_IQueue=[self.object_skyrms], 
-                                               MATCH_KERNEL=MK, 
-                                               REAL_OUTPUT=False, 
-                                               REAL_OUTPUT_SIZE=None, 
-                                               NORMALIZE_OUTPUT=True, 
-                                               VERBOSE_LEVEL=2)
-        self.FKDECO = self.op_array(self.FKDECO, dtype=np.complex128)
+        self.FKDECO = DeCorrelation_Calculator(
+            NX_IMG=N0,
+            NY_IMG=N1,
+            KERNEL_JQueue=[self.op.asnumpy(self.PSF_resamp_object)],
+            BKGSIG_JQueue=[self.target_skyrms],
+            KERNEL_IQueue=[self.op.asnumpy(self.PSF_target)],
+            BKGSIG_IQueue=[self.object_skyrms],
+            MATCH_KERNEL=MK,
+            REAL_OUTPUT=False,
+            REAL_OUTPUT_SIZE=None,
+            NORMALIZE_OUTPUT=True,
+            VERBOSE_LEVEL=2,
+        )
+        self.FKDECO = self.op.array(self.FKDECO, dtype=np.complex128)
         print("Decorrelaton kernel calculated.")
-    
-    def apply_decorrelation( self, img ):
+
+    def apply_decorrelation(self, img):
         # do decorrelation
 
         # decorrelate difference image
-        _img = self.op_asnumpy(img)
+        _img = self.op.asnumpy(img)
         if _img.shape == self.FKDECO.shape:
             FPixA = np.fft.fft2(_img)
             PixA_decorr = np.fft.ifft2(FPixA * self.FKDECO).real
-            decorimg = self.op_array(PixA_decorr, dtype=np.float64)
+            decorimg = self.op.array(PixA_decorr, dtype=np.float64)
         else:
             NK0, NK1 = _img.shape
             N0, N1 = self.FKDECO.shape
             KERN_CSZ = KERNEL_CSZ(KERNEL=_img, NX_IMG=N0, NY_IMG=N1)
             FKERN_decorr = np.fft.fft2(KERN_CSZ) * self.FKDECO
             PixA_KERN_decorr = KERNEL_CSZ_INV(np.fft.ifft2(FKERN_decorr).real, NX_KERN=NK0, NY_KERN=NK1)
-            decorimg = self.op_array(PixA_KERN_decorr, dtype=np.float64)
+            decorimg = self.op.array(PixA_KERN_decorr, dtype=np.float64)
         return decorimg
-    
-    def create_score_image( self ):        
+
+    def create_score_image(self):
         # retrieve the decorrelated PSF
         # Note: here we assume the same pixel size for PSF and imgaes.
         NX, NY = self.PixA_target.shape
-        PSF_object_CSZ = self.op_KERNEL_CSZ(KERNEL=self.PSF_object, NX_IMG=NX, NY_IMG=NY)
-        PSF_target_CSZ = self.op_KERNEL_CSZ(KERNEL=self.PSF_target, NX_IMG=NX, NY_IMG=NY)
-        FPSF_dDIFF = self.op_fft.fft2(PSF_object_CSZ) * self.op_fft.fft2(PSF_target_CSZ) * self.FKDECO
+        PSF_object_CSZ = self.op.KERNEL_CSZ(KERNEL=self.PSF_object, NX_IMG=NX, NY_IMG=NY)
+        PSF_target_CSZ = self.op.KERNEL_CSZ(KERNEL=self.PSF_target, NX_IMG=NX, NY_IMG=NY)
+        FPSF_dDIFF = self.op.fft.fft2(PSF_object_CSZ) * self.op.fft.fft2(PSF_target_CSZ) * self.FKDECO
 
-        # apply the decorrelation on difference image again (redundant, a workaround) 
-        FPixA_DIFF = self.op_fft.fft2( self.PixA_DIFF )
+        # apply the decorrelation on difference image again (redundant, a workaround)
+        FPixA_DIFF = self.op.fft.fft2(self.PixA_DIFF)
         FPixA_dDIFF = FPixA_DIFF * self.FKDECO
 
-        FPixA_SCORE = FPixA_dDIFF * self.op_conj(FPSF_dDIFF)
-        PixA_SCORE = self.op_fft.ifft2(FPixA_SCORE).real
+        FPixA_SCORE = FPixA_dDIFF * self.op.conj(FPSF_dDIFF)
+        PixA_SCORE = self.op.fft.ifft2(FPixA_SCORE).real
 
         # an ad-hoc correction to make score image has standrd Gaussian distribution at background
-        skysig_SCORE = SkyLevel_Estimator.SLE(PixA_obj=self.op_asnumpy(PixA_SCORE))[1]
+        skysig_SCORE = SkyLevel_Estimator.SLE(PixA_obj=self.op.asnumpy(PixA_SCORE))[1]
         PixA_SCORE /= skysig_SCORE
 
         return PixA_SCORE
 
-    def create_variance_image( self ):
+    def create_variance_image(self):
 
-        assert self.PixA_targetVar.flags['C_CONTIGUOUS']
-        assert self.PixA_resamp_objectVar.flags['C_CONTIGUOUS']
+        assert self.PixA_targetVar.flags["C_CONTIGUOUS"]
+        assert self.PixA_resamp_objectVar.flags["C_CONTIGUOUS"]
 
         # calculate variance image for (un-decorrelated) difference image
         NX, NY = self.PixA_target.shape
-        PSF_resamp_object_CSZ = self.op_KERNEL_CSZ(KERNEL=self.PSF_resamp_object, NX_IMG=NX, NY_IMG=NY)
-        PSF_target_CSZ = self.op_KERNEL_CSZ(KERNEL=self.PSF_target, NX_IMG=NX, NY_IMG=NY)
+        PSF_resamp_object_CSZ = self.op.KERNEL_CSZ(KERNEL=self.PSF_resamp_object, NX_IMG=NX, NY_IMG=NY)
+        PSF_target_CSZ = self.op.KERNEL_CSZ(KERNEL=self.PSF_target, NX_IMG=NX, NY_IMG=NY)
 
         # Note: let's skip the matching kernel here, as it is expected to be a minor compensation.
-        PixA_dDIFFVar = self.op_fft.ifft2(
-            self.op_fft.fft2(self.PixA_resamp_objectVar) * \
-            self.op_fft.fft2((self.op_fft.ifft2(self.op_fft.fft2(PSF_target_CSZ) * self.FKDECO)).real**2)
+        PixA_dDIFFVar = self.op.fft.ifft2(
+            self.op.fft.fft2(self.PixA_resamp_objectVar)
+            * self.op.fft.fft2((self.op.fft.ifft2(self.op.fft.fft2(PSF_target_CSZ) * self.FKDECO)).real ** 2)
         ).real
-        PixA_dDIFFVar += self.op_fft.ifft2(
-            self.op_fft.fft2(self.PixA_targetVar) * \
-            self.op_fft.fft2((self.op_fft.ifft2(self.op_fft.fft2(PSF_resamp_object_CSZ) * self.FKDECO)).real**2)
+        PixA_dDIFFVar += self.op.fft.ifft2(
+            self.op.fft.fft2(self.PixA_targetVar)
+            * self.op.fft.fft2((self.op.fft.ifft2(self.op.fft.fft2(PSF_resamp_object_CSZ) * self.FKDECO)).real ** 2)
         ).real
 
         return PixA_dDIFFVar
-    
+
     # Do we need this?  We should just unreference the object
-    def cleanup( self ):
+    def cleanup(self):
         pass
